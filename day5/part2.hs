9ec3d3eb726e14861809bfb9fc5ef4e98560222e
Pontus Malm <malm@chalmers.it>
2017-12-05 13:40:18 +0100

    Simplified file read

--- day5/part2.hs
+++ day5/part2.hs
@@ -5,9 +5,6 @@ import qualified Data.Map as M
 
 -- Compile this program with ghc before you run it
 
-strToInt :: String -> Int
-strToInt s = read s
-
 jumping :: M.Map Int Int -> Int -> Int -> Int
 jumping list pos steps | jump + pos >= M.size list = steps + 1
                        | jump > 2 = jumping (M.insert pos (jump-1) list) (pos + jump) (steps+1)
@@ -18,6 +15,6 @@ jumping list pos steps | jump + pos >= M.size list = steps + 1
 main :: IO()
 main = do
     input <- readFile "input"
-    let contents = map strToInt (lines input)
+    let contents = map read (lines input)
     let mymap = M.fromList $ zip [0..length contents] (contents)
     putStrLn $ show $ jumping mymap 0 0
