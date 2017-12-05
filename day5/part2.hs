19916b5349e60714f5a5910981a47e0269f25f9c
Pontus Malm <malm@chalmers.it>
2017-12-05 12:34:16 +0100

    Prettier code

--- day5/part2.hs
+++ day5/part2.hs
@@ -8,19 +8,16 @@ import qualified Data.Map as M
 strToInt :: String -> Int
 strToInt s = read s
 
-unsafePos :: Maybe Int -> Int
-unsafePos (Just n) = n
-
-jumping :: M.Map Int Int -> Int -> Int -> Int -> Int
-jumping list pos length steps | jump + pos >= length = steps + 1
-                       | jump > 2 = jumping (M.insert pos (jump-1) list) (pos + jump) length (steps+1)
-                       | otherwise = jumping (M.insert pos (jump+1) list) (pos + jump) length (steps+1)
+jumping :: M.Map Int Int -> Int -> Int -> Int
+jumping list pos steps | jump + pos >= M.size list = steps + 1
+                       | jump > 2 = jumping (M.insert pos (jump-1) list) (pos + jump) (steps+1)
+                       | otherwise = jumping (M.insert pos (jump+1) list) (pos + jump) (steps+1)
          where
-            jump = unsafePos $ M.lookup pos list
+            jump = list M.! pos
 
 main :: IO()
 main = do
     input <- readFile "input"
     let contents = map strToInt (lines input)
     let mymap = M.fromList $ zip [0..length contents] (contents)
-    putStrLn $ show $ jumping mymap 0 (length contents) 0
+    putStrLn $ show $ jumping mymap 0 0
